713af459821db82b3f165c381e118da9bfc17b49
kamran-8674 <connect4kamran@gmail.com>
2026-04-18 11:30:18 +0600

    working on users management page

--- src/pages/dahsboard/users management/UsersManagement.jsx
+++ src/pages/dahsboard/users management/UsersManagement.jsx
@@ -1,10 +1,107 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const UsersManagement = () => {
+
+    const axiosSecure = useAxiosSecure()
+    const {data: users =[], refetch} = useQuery({
+        queryKey: ['users'],
+        queryFn: async()=>{
+            const res = await axiosSecure.get('/users')
+            return res.data
+        }
+    })
+
+   const handleMakeAdmin = (user) =>{
+    const roleInfo = {role: 'admin'}
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+    .then(res=>{
+        if(res.data.modifiedCount){
+            refetch()
+            
+            console.log('user updated')
+        }
+    })
+   }
+   const handleRemoveAdmin = (user) =>{
+    const roleInfo = {role: 'user'}
+    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+    .then(res=>{
+        if(res.data.modifiedCount){
+            refetch()
+            
+            console.log('user updated')
+        }
+    })
+   }
+
+
     return (
-        <div>
-            vvvv
-        </div>
+         <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">
+        Manage Users
+      </h1>
+
+      <div className="overflow-x-auto">
+        <table className="table table-zebra">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.displayName}</td>
+                <td>{user.email}</td>
+
+                <td>
+                  <span
+                    className={`badge ${
+                      user.role === "admin"
+                        ? "badge-success"
+                        : user.role === "decorator"
+                        ? "badge-info"
+                        : "badge-ghost"
+                    }`}
+                  >
+                    {user.role || "user"}
+                  </span>
+                </td>
+
+                <td className="space-x-2">
+                  
+                  {/* Make Admin */}
+                  {user.role === 'admin' ?  <button
+                    onClick={() => handleRemoveAdmin(user)}
+                    className="btn btn-xs btn-primary"
+                  >
+                    Remove Admin
+                  </button>: 
+                   <button
+                    onClick={() => handleMakeAdmin(user)}
+                    className="btn btn-xs btn-success"
+                  >
+                    Make Admin
+                  </button>
+                  
+                  }
+                 
+
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
     );
 };
 
